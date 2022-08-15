210109b9b6af406dc6b6b819e7b164542ce648ef
Jose De la Paz Noguera <Jose.DelaPazNoguera@arup.com>
2022-08-15 17:19:42 +0100

    upgrade station socres

--- main.py
+++ main.py
@@ -60,12 +60,6 @@ destination_score = base_df.Destination_Category.map(my_dict)
 base_df['origin_score'] = origin_score
 base_df['destination_score'] = destination_score
 
-# Replace alphabetical categories by a numerical score based on a dictionary
-# base_df = my_df
-# my_dict = {'0': 0,'A': 1, 'B': 2, 'B1': 3, 'B2': 4, 'B3': 5, 'C': 6, 'Null': -1}
-# base_df.Origin_Category = base_df.Origin_Category.map(my_dict)
-# base_df.Destination_Category = base_df.Destination_Category.map(my_dict)
-
 # Remove OD pairs where the score is 0 or Null. Save the number of removed records for traceability
 dropped_origins = base_df.origin_score[base_df.origin_score < 1].count()
 dropped_destinations = base_df.destination_score[base_df.destination_score < 1].count()
@@ -76,10 +70,16 @@ base_df = base_df.drop(base_df[base_df.destination_score < 1].index)
 print(f"A total of {dropped_origins:n} origins and {dropped_destinations:n} destinations were invalid and not included in the analysis.")
 
 # Categorize the journeys by looking at the maximum numerical score of the origin and destination stations
-jny_category = np.maximum(base_df.origin_score, base_df.destination_score)
+jny_score = np.maximum(base_df.origin_score, base_df.destination_score)
 
 # Add the list as a new column to the existing dataframe
+base_df['jny_score'] = jny_score
+my_dict_2 = {1: 'A', 2: 'B', 3: 'B1', 4: 'B2', 5: 'B3', 6: 'C'}
+jny_category = base_df.jny_score.map(my_dict_2)
 base_df['jny_category'] = jny_category
+
+print(base_df.tail(10))
+
 
 #base_df.info()
 v1 = base_df.groupby("jny_category").Total_Journeys.sum()
@@ -91,14 +91,27 @@ print(type(v1))
 
 # Import stations to be upgraded
 input_path = r'C:/Users/jose.delapaznoguera/Projects/NRAS/Data\Input template.csv'
-upgrade_df = pd.read_csv(input_path)
+upgrade_list = pd.read_csv(input_path)
 
 # Transform the list into a dataframe
-upgrade_df.columns = [c.replace(' ','_') for c in upgrade_df.columns]
-print(upgrade_df.head())
+upgrade_list.columns = [c.replace(' ','_') for c in upgrade_list.columns]
+print(upgrade_list.head())
+
+# Create a copy of the base data frame
+scenario_1 = base_df
 
 # Next steps:
 # 1. find the stations to be upgraded.
+for station in upgrade_list.TLC:
+    scenario_1['Origin_TLC' == station].Origin_Category = upgrade_list['TLC' == station].New_Category
+    scenario_1['Origin_TLC' == station].origin_score = scenario_1.Origin_Category.map(my_dict)
+    scenario_1['Destination_TLC' == station].Destination_Category = upgrade_list['TLC' == station].New_Category
+    scenario_1['Destination_TLC' == station].destination_score = scenario_1.Destination_Category.map(my_dict)
+    scenario_1['Origin_TLC' == station].jny_score = np.maximum(scenario_1.origin_score, scenario_1.destination_score)
+    scenario_1['Destination_TLC' == station].jny_score = np.maximum(scenario_1.origin_score, scenario_1.destination_score)
+
+print(scenario_1['Origin_TLC' == "HUR"])
+
 # 2. upgrade the category, the station score and the journey score
 # 3. generate the new OD matrix. It must have the same format as the original OD matrix
 # 4. calculate the network scores before and after
