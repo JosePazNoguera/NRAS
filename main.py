89832020399035d416ae01950b059f01f0359954
kharesa-arup <43879855+kharesa-arup@users.noreply.github.com>
2022-09-27 15:38:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ This script will attempt to replicate the actions of the spreadsheet generating 
 '''
 
 
-from asyncio.unix_events import _UnixSelectorEventLoop
-from email import header
+#from asyncio.unix_events import _UnixSelectorEventLoop
+#from email import header
 from operator import index
 from matplotlib.pyplot import polar
 import pandas as pd, numpy as np, glob, ast, openpyxl, shutil, pyodbc, random, datetime
@@ -24,7 +24,7 @@ def get_orr_step_free_category(search_value):
     return orr[search_value]
 
 def get_updated_stations():
-    input_path = '/Users/kharesa-kesa.spencer/Library/CloudStorage/OneDrive-Arup/Projects/Network Rail Accessibility case/matrices/Input template.csv'
+    input_path = r'C:\Users\Kharesa-Kesa.Spencer\OneDrive - Arup\Projects\Network Rail Accessibility case\matrices\Input template.csv'
     input_df = pd.read_csv(input_path)
 
     return input_df
@@ -36,7 +36,13 @@ def get_mobility_isolation_matrix(search_value):
     return e[search_value]
 
 def get_list_col():
-    list_cols = ['2019_Potential_Unlocked_Rank',
+    list_cols = ['Station_Name', 'Unique_Code', 'Station_Facility_Owner',
+       'Network_Rail_Region', 'ORR_Step_Free_Category', 'DfT_Category',
+       'Inaccessible_(1_if_not_Step_Free_Cat._A_or_B1)',
+       '2018/2019_ORR_Total_Entries/Exits_and_Interchange',
+       '2019_Journeys_to_an_accessible_destination',
+       '2019_Journeys_from_an_accessible_origin',
+       '2019_Total_Unlocked_Journeys', '2019_Potential_Unlocked_Rank',
        '2019_Unlocked_Journeys_Percentile',
        '2019_Unlocked_Journeys_Matrix_Outcome',
        '2019_Connectivity_(count_of_stations_directly_served)',
@@ -56,10 +62,49 @@ def get_list_col():
        'Region_and_Final_Score', 'Journeys_and_Final_Score',
        'Region_and_Local_Factor', 'Score_Change']
 
-    return list_cols
+    one = ['Station_Name', 'Unique_Code', 'Station_Facility_Owner',
+       'Network_Rail_Region', 'ORR_Step_Free_Category', 'DfT_Category',
+       'Inaccessible_(1_if_not_Step_Free_Cat._A_or_B1)',
+       '2018/2019_ORR_Total_Entries/Exits_and_Interchange',
+       '2019_Journeys_to_an_accessible_destination',
+       '2019_Journeys_from_an_accessible_origin',
+       '2019_Total_Unlocked_Journeys', '2019_Potential_Unlocked_Rank',
+       '2019_Unlocked_Journeys_Percentile',
+       '2019_Unlocked_Journeys_Matrix_Outcome',
+       '2019_Connectivity_(count_of_stations_directly_served)',
+       '2019_Connectivity_Rank', '2019_Connectivity_Percentile']
 
-def get_new_categories_set_jrnys(base_df):
+    two = ['Inaccessible_(1_if_not_Step_Free_Cat._A_or_B1)','2019_Connectivity_Matrix_Outcome',
+       'Connectivity_and_Journeys_Matrix_Outcome',
+       'Connectivity_and_Journeys_Matrix_Outcome.1', 'Mobility_Score',
+       'Isolation_(1_if_no_Cat_A_in_20_mins_drive_isochrone)',
+       'Additional_Flags', 'Original_Isolation_Score',
+       'Revisited_Isolation_score', 'Mobility/Isolation',
+       'Isolation_and_Current_Access_Matrix_Outcome', 'Socioeconomic_Flags',
+       'Socioeconomic_classification', 'Local_Impact_Score',
+       'Local_Impact_Classification', 'Socioecon_/_Local_Impact',
+       'Socioeconomic_/_Local_Matrix_outcome', 'Average_of_two_scores',
+       'Score__without_modifier', 'Base_score_+_modifiers',
+       'Score_with_modifier', 'Footfall_Modifier', 'Final_Outcome', 'Change',
+       'Region_and_Final_Score', 'Journeys_and_Final_Score',
+       'Region_and_Local_Factor', 'Score_Change']
 
+    three = ['Isolation_(1_if_no_Cat_A_in_20_mins_drive_isochrone)',
+       'Additional_Flags', 'Original_Isolation_Score',
+       'Revisited_Isolation_score', 'Mobility/Isolation',
+       'Isolation_and_Current_Access_Matrix_Outcome', 'Socioeconomic_Flags',
+       'Socioeconomic_classification', 'Local_Impact_Score',
+       'Local_Impact_Classification', 'Socioecon_/_Local_Impact',
+       'Socioeconomic_/_Local_Matrix_outcome', 'Average_of_two_scores',
+       'Score__without_modifier', 'Base_score_+_modifiers',
+       'Score_with_modifier', 'Footfall_Modifier', 'Final_Outcome', 'Change',
+       'Region_and_Final_Score', 'Journeys_and_Final_Score',
+       'Region_and_Local_Factor', 'Score_Change']
+
+
+    return three
+
+def input_OD_Matrix():
 
     #inputs
     #temporary variable to direct the code, when the access database is updated then we can remove
@@ -80,12 +125,15 @@ def get_new_categories_set_jrnys(base_df):
 
     else:
         #reading the data in from CSVs manaually from local storage
-        vs1 = pd.read_csv('/Users/kharesa-kesa.spencer/Library/CloudStorage/OneDrive-Arup/Projects/Network Rail Accessibility case/matrices/Vector save.csv')
-        vs2 = pd.read_csv('/Users/kharesa-kesa.spencer/Library/CloudStorage/OneDrive-Arup/Projects/Network Rail Accessibility case/matrices/Vector save 2.csv')
+        vs1 = pd.read_csv(r'C:\Users\Kharesa-Kesa.Spencer\OneDrive - Arup\Projects\Network Rail Accessibility case\matrices\Vector save.csv')
+        vs2 = pd.read_csv(r'C:\Users\Kharesa-Kesa.Spencer\OneDrive - Arup\Projects\Network Rail Accessibility case\matrices\Vector save 2.csv')
         frames = [vs1,vs2]
         OD_df = pd.concat(frames, ignore_index=True)
 
+    return OD_df
 
+def map_input_stations(OD_df, base_df):
+    
     # Add numerical score based on a dictionary
     my_dict = {'0': 0,'A': 1, 'B': 2, 'B1': 3, 'B2': 4, 'B3': 5, 'C': 6, 'Null': -1}
     origin_score = OD_df.Origin_Category.map(my_dict)
@@ -99,7 +147,7 @@ def get_new_categories_set_jrnys(base_df):
     OD_df = OD_df.drop(OD_df[OD_df.origin_score < 1].index)
     OD_df = OD_df.drop(OD_df[OD_df.destination_score < 1].index)
 
-    print(f"A total of {dropped_origins:n} origins and {dropped_destinations:n} destinations were invalid and not included in the analysis.")
+    #print(f"A total of {dropped_origins:n} origins and {dropped_destinations:n} destinations were invalid and not included in the analysis.")
 
     # Categorize the journeys by looking at the maximum numerical score of the origin and destination stations
     jny_score = np.maximum(OD_df.origin_score, OD_df.destination_score)
@@ -117,13 +165,13 @@ def get_new_categories_set_jrnys(base_df):
     #print(type(v1))
     #print(base_df.tail(10))
 
-
-
     #this method does all the calculating the new categories
 
     # Import stations to be upgraded
     upgrade_list = get_updated_stations()
 
+    #In anticipation of the new score the columm is set to None
+    base_df['Inaccessible_(1_if_not_Step_Free_Cat._A_or_B1)'] = None
 
 
     for tlc in upgrade_list.TLC:
@@ -140,6 +188,9 @@ def get_new_categories_set_jrnys(base_df):
         if tlc in base_df.values:
             # Update ORR_Step_Free_Category
             base_df.loc[base_df.Unique_Code == str(tlc), 'ORR_Step_Free_Category'] = new_category
+            #setting their indicator to 0 as they are now accessible
+            base_df.loc[base_df.Unique_Code == str(tlc), 'Inaccessible_(1_if_not_Step_Free_Cat._A_or_B1)'] = 0
+
 
     # Update origin score
     OD_df.origin_score = OD_df.Origin_Category.map(my_dict)
@@ -178,11 +229,19 @@ def get_new_categories_set_jrnys(base_df):
             base_df.loc[base_df.Unique_Code == str(code), '2019_Journeys_to_an_accessible_destination'] = total_jd
 
 
+    return base_df
+
+def get_new_categories_set_jrnys(base_df):
+
+    #set the OD matrix, either from the access database or from the csv
+    OD_df = input_OD_Matrix()
+
+    base_df = map_input_stations(OD_df, base_df)
+
     #so now the base df and the od df both have updated station categories from the input template
     #next is adding the column codes and the journey stats, mapping from the OD_df to the base df
-    #all stations are set to 1, then if they're accessible they're changed to 0 and if the column is empty its set to NaN
+    # if a station is accessible they're changed to 0 and if the column is empty its set to NaN
     #Did it manually due to error
-    base_df['Inaccessible_(1_if_not_Step_Free_Cat._A_or_B1)'] = None
     base_df.loc[base_df.ORR_Step_Free_Category == 'A', 'Inaccessible_(1_if_not_Step_Free_Cat._A_or_B1)'] = 0
     base_df.loc[base_df.ORR_Step_Free_Category == 'B1', 'Inaccessible_(1_if_not_Step_Free_Cat._A_or_B1)'] = 0
     base_df.loc[base_df.ORR_Step_Free_Category == 'B', 'Inaccessible_(1_if_not_Step_Free_Cat._A_or_B1)'] = 1
@@ -192,18 +251,48 @@ def get_new_categories_set_jrnys(base_df):
     
     base_df.loc[base_df.ORR_Step_Free_Category.isna(), 'Inaccessible_(1_if_not_Step_Free_Cat._A_or_B1)'] = np.NaN
 
-    #totals
-    base_df['2019_Total_Unlocked_Journeys'] = base_df['2019_Journeys_to_an_accessible_destination'] + base_df['2019_Journeys_from_an_accessible_origin']
-    base_df['2019_Potential_Unlocked_Rank'] = base_df['2019_Total_Unlocked_Journeys'].rank(ascending=False)
-    base_df['2019_Unlocked_Journeys_Percentile'] = base_df['2019_Total_Unlocked_Journeys'].rank(ascending=False, pct=True)
 
 
-    #setting this by looping through df with if statements
+    unlock_jrny = []
+    unlocked_jrny_perc = []
+    connc = []
+    connc_rank = []
+    connc_rank_perc = []
+    connc_jrnys_matrx = []
+
     matrix_outcome_list = []
+    matrix_outcome_list_2 = []
 
-    for val in base_df['Inaccessible_(1_if_not_Step_Free_Cat._A_or_B1)']:
+    for index, row in base_df.iterrows():
 
-        if  val == 1:
+        if row['Inaccessible_(1_if_not_Step_Free_Cat._A_or_B1)'] == 1:
+
+            unlock_jrny.append(int(row['2019_Journeys_to_an_accessible_destination'] + row['2019_Journeys_from_an_accessible_origin']))
+            connc.append(int(row['2019_Connectivity_(count_of_stations_directly_served)']))
+
+
+            #setting this by looping through df with if statements
+        else:
+            unlock_jrny.append(None)
+            connc.append(None)
+
+        
+    base_df['2019_Total_Unlocked_Journeys'] = unlock_jrny
+    unlock_jrny_s = pd.Series(unlock_jrny)
+    base_df['2019_Potential_Unlocked_Rank'] = unlock_jrny_s.rank(ascending=False)
+    base_df['2019_Unlocked_Journeys_Percentile'] = unlock_jrny_s.rank(ascending=False, pct=True)
+    base_df['2019_Connectivity_(count_of_stations_directly_served)'] = connc
+    connc_s = pd.Series(connc)
+    base_df['2019_Connectivity_Rank'] = connc_s.rank(ascending=False)
+    base_df['2019_Connectivity_Percentile'] = connc_s.rank(ascending=False, pct=True)
+
+
+    
+    for index, row in base_df.iterrows():
+        val = row['2019_Unlocked_Journeys_Percentile']
+        val_2 = row['2019_Connectivity_Percentile']
+            
+        if row['Inaccessible_(1_if_not_Step_Free_Cat._A_or_B1)'] == 1:
 
             if val< 0.33:
                 matrix_outcome_list.append('Top')
@@ -217,35 +306,30 @@ def get_new_categories_set_jrnys(base_df):
         else:
             matrix_outcome_list.append('')
 
-    base_df['2019_Unlocked_Journeys_Matrix_Outcome'] = matrix_outcome_list
-    base_df['2019_Connectivity_Rank'] = base_df['2019_Connectivity_(count_of_stations_directly_served)'].rank(ascending=False)
-    base_df['2019_Connectivity_Percentile'] = base_df['2019_Connectivity_(count_of_stations_directly_served)'].rank(ascending=False, pct=True)
 
-    matrix_outcome_list_2 = []
+        if row['Inaccessible_(1_if_not_Step_Free_Cat._A_or_B1)'] == 1:
 
-    for val in base_df['Inaccessible_(1_if_not_Step_Free_Cat._A_or_B1)']:
-
-        if  val == 1:
-
-            if val< 0.33:
+            if val_2 < 0.33:
                 matrix_outcome_list_2.append('Top')
 
-            elif val < 0.66:
+            elif val_2 < 0.66:
                 matrix_outcome_list_2.append('Middle')
-            
+                
             else:
                 matrix_outcome_list_2.append('Bottom')
         
         else:
             matrix_outcome_list_2.append('')
+            
+
 
     
+    base_df['2019_Unlocked_Journeys_Matrix_Outcome'] = matrix_outcome_list
     base_df['2019_Connectivity_Matrix_Outcome'] = matrix_outcome_list_2
     base_df['Connectivity_and_Journeys_Matrix_Outcome'] = base_df['2019_Unlocked_Journeys_Matrix_Outcome']+base_df['2019_Connectivity_Matrix_Outcome']
 
     #column u not done
 
-    
     return base_df
 
 def set_mobility_isolation_score(updated_cats_and_jrnys, alt_any_df):
@@ -298,7 +382,7 @@ def set_mobility_isolation_score(updated_cats_and_jrnys, alt_any_df):
 
         if target in updated_cats_and_jrnys.values:
             
-            updated_cats_and_jrnys.loc[updated_cats_and_jrnys.Unique_Code == target, 'Revisited_Isolation_score'] = 'Bottom'
+            updated_cats_and_jrnys.loc[(updated_cats_and_jrnys.Unique_Code == target) & (updated_cats_and_jrnys['Inaccessible_(1_if_not_Step_Free_Cat._A_or_B1)']==1), 'Revisited_Isolation_score'] = 'Bottom'
 
     
     #COLUMN Y
@@ -315,45 +399,41 @@ def set_mobility_isolation_score(updated_cats_and_jrnys, alt_any_df):
             updated_cats_and_jrnys.loc[updated_cats_and_jrnys['Mobility/Isolation']==mob, 'Isolation_and_Current_Access_Matrix_Outcome'] = get_mobility_isolation_matrix(mob)
 
     
+    #dropping final duplicates before returning
+    updated_cats_and_jrnys = updated_cats_and_jrnys.loc[:,~updated_cats_and_jrnys.columns.duplicated()].copy()
 
     return updated_cats_and_jrnys
 
 def blanking_rows(updated_mobility_and_isolation):
 
-    list_cols = get_list_col()
+    list_of_cols = get_list_col()
 
     for index, row in updated_mobility_and_isolation.iterrows():
 
-        if row['Inaccessible_(1_if_not_Step_Free_Cat._A_or_B1)'] == 1:
+        for col in list_of_cols:
 
-            updated_mobility_and_isolation.loc[updated_mobility_and_isolation['Inaccessible_(1_if_not_Step_Free_Cat._A_or_B1)']==1, list_cols] = None
+            if row['Inaccessible_(1_if_not_Step_Free_Cat._A_or_B1)'] == 0:
+
+                updated_mobility_and_isolation[col] = None
 
 
     return updated_mobility_and_isolation
 
 
 
-def main():
-    #any variables for the main 
+path_of_spreadsh = r'C:\Users\Kharesa-Kesa.Spencer\OneDrive - Arup\Projects\Network Rail Accessibility case\CSV WORK\Step Free Scoring_JDL_v3.00.xlsx'
+base_df  = pd.read_excel(path_of_spreadsh, sheet_name = "All Stations", header=2 , usecols="B:AS", engine='openpyxl')
+alt_any  = pd.read_excel(path_of_spreadsh, sheet_name = "Alt_Any_20", header=4 , usecols="B:F", engine='openpyxl')
 
-    path_of_spreadsh = '/Users/kharesa-kesa.spencer/Library/CloudStorage/OneDrive-Arup/Projects/Network Rail Accessibility case/CSV WORK/Step Free Scoring_JDL_v3.00.xlsx'
-    base_df  = pd.read_excel(path_of_spreadsh, sheet_name = "All Stations", header=2 , usecols="B:AS", engine='openpyxl')
-    alt_any  = pd.read_excel(path_of_spreadsh, sheet_name = "Alt_Any_20", header=4 , usecols="B:F", engine='openpyxl')
-
-    base_df.columns = [c.replace(' ','_') for c in base_df.columns]
-    alt_any.columns = [c.replace(' ','_') for c in alt_any.columns]
+base_df.columns = [c.replace(' ','_') for c in base_df.columns]
+alt_any.columns = [c.replace(' ','_') for c in alt_any.columns]
 
 
 
-    updated_cats_and_jrnys = get_new_categories_set_jrnys(base_df)
+updated_cats_and_jrnys = get_new_categories_set_jrnys(base_df)
 
-    updated_mobility_and_isolation = set_mobility_isolation_score(updated_cats_and_jrnys, alt_any)
+updated_mobility_and_isolation = set_mobility_isolation_score(updated_cats_and_jrnys, alt_any)
 
-    updated_columns = blanking_rows(updated_mobility_and_isolation)
-
-    updated_columns.head(20)
+final_df = blanking_rows(updated_mobility_and_isolation)
 
 
-
-if __name__ == "__main__":
-    main()
